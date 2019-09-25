9a6880cdf7ff165d5aa261ecf1f6bac9260c1fc0
andres.serrano <aserrano@rccl.com>
2019-09-24 22:46:57 -0500

    Add Taps Layout and Basic Design
    Navigation Component  (https://codelabs.developers.google.com/codelabs/android-navigation/index.html?index=..%2F..index#0)
    
    ViewPager2  (https://developer.android.com/jetpack/androidx/releases/viewpager2)
    
    Data Binding (https://codelabs.developers.google.com/codelabs/android-databinding/index.html?index=..%2F..index#5)
    
    Test it on
    Pixel 2XL android 10

---
+++ app/src/main/java/com/puzzle/bench/post_aac/HomeViewPagerFragment.kt
@@ -0,0 +1,46 @@
+package com.puzzle.bench.post_aac
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import com.puzzle.bench.post_aac.adapter.MAIN_POST_LIST
+import com.puzzle.bench.post_aac.adapter.MY_FAVORITE_POST
+import com.puzzle.bench.post_aac.adapter.PostPagerAdapter
+import com.puzzle.bench.post_aac.databinding.ViewPagerFragmentBinding
+
+class HomeViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = ViewPagerFragmentBinding.inflate(inflater, container, false)
+        val tabLayout = binding.tabs
+        val viewPager = binding.viewPager
+
+        viewPager.adapter = PostPagerAdapter(this)
+
+        // Set the icon and text for each tab
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = getTabTitle(position)
+        }.attach()
+
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+
+        return binding.root
+    }
+
+
+    private fun getTabTitle(position: Int): String? {
+        return when (position) {
+            MAIN_POST_LIST -> getString(R.string.tap_title_all_post)
+            MY_FAVORITE_POST -> getString(R.string.tap_title_favorite)
+            else -> null
+        }
+    }
+}
