9b0251037c3c21b8eb273c00d62588e96ff0bbb7
andres.serrano <aserrano@rccl.com>
2019-09-25 12:47:15 -0500

    add service call post using coroutines
    https://codelabs.developers.google.com/codelabs/kotlin-coroutines/index.html?index=..%2F..index#0
    https://medium.com/exploring-android/android-networking-with-coroutines-and-retrofit-a2f20dd40a83

--- app/src/main/java/com/puzzle/bench/post_aac/presentation/di/ViewModelInjector.kt
+++ app/src/main/java/com/puzzle/bench/post_aac/presentation/di/ViewModelInjector.kt
@@ -1,11 +1,12 @@
 package com.puzzle.bench.post_aac.presentation.di
 
 import com.puzzle.bench.post_aac.data.DataSourcePostImpl
+import com.puzzle.bench.post_aac.data.networking.RetrofitClient.Factory.makeServiceApi
 import com.puzzle.bench.post_aac.presentation.viewmodels.AllPostViewModelFactory
 
 object ViewModelInjector {
 
-    private fun getAllPost() = DataSourcePostImpl()
+    private fun getAllPost() = DataSourcePostImpl(makeServiceApi())
 
     fun provideAllPostViewModel() = AllPostViewModelFactory(getAllPost())
 }
