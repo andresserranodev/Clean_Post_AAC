d3c5159818ebabfb3fbca48f25dd6c4d2c88c62e
andres.serrano <aserrano@rccl.com>
2019-09-28 20:20:26 -0500

    fix issue app resiliency cases 1 code factor issue

--- app/src/main/java/com/puzzle/bench/post_aac/data/FetchCommentByPostIdImpl.kt
+++ app/src/main/java/com/puzzle/bench/post_aac/data/FetchCommentByPostIdImpl.kt
@@ -5,28 +5,19 @@ import com.puzzle.bench.post_aac.data.networking.retrofit.JsonPlaceholderApi
 import com.puzzle.bench.post_aac.model.FetchCommentsState
 
 const val NO_ERROR = ""
-const val NO_RECORDS_FOUND = "No records found"
 
 class FetchCommentByPostIdImpl(
     private val jsonPlaceholderApi: JsonPlaceholderApi,
     private val commentMapper: CommentMapper
 ) : FetchCommentByPostId {
     override suspend fun fetchCommentByPostId(postId: Int): FetchCommentsState {
-        try {
-            val response = jsonPlaceholderApi.getAllCommentsRequest(postId)
-
-            val listComments = response.let { listCommentsResponse ->
-                listCommentsResponse.map { commentMapper.transformServiceToEntity(it) }
+        return try {
+            val response = jsonPlaceholderApi.getAllCommentsRequest(postId).map {
+                commentMapper.transformServiceToEntity(it)
             }
-            return if (listComments.isEmpty()) {
-                FetchCommentsState(NO_RECORDS_FOUND, listOf())
-
-            } else {
-                FetchCommentsState(NO_ERROR, listComments)
-            }
-
+            FetchCommentsState(NO_ERROR, response)
         } catch (ex: Exception) {
-            return FetchCommentsState(ex.message.toString(), listOf())
+            FetchCommentsState(ex.message.toString(), listOf())
         }
 
     }
